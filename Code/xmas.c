c10faca1de176d50f09751240d2c2f4a04d7b28b
Wirewords-PC <sarathm00@gmail.com>
2015-12-17 17:13:54 +0530

    Final

--- Code/xmas.c
+++ Code/xmas.c
@@ -2,12 +2,42 @@
 #pragma config WDT = OFF
 
 #define DEL 5
-#define SW1 PORTDbits.RD0
-#define SW2 PORTDbits.RD1
+#define LED PORTBbits.RB7
 
 void pattern1();
 void pattern2();
 void pattern3();
+void inter_init();
+
+int flag = 0;
+int sel=0;
+
+void inter_init()
+{
+	INTCONbits.INT0IF = 0;
+	INTCONbits.INT0IE = 1;
+	INTCONbits.GIE = 1;
+}
+
+#pragma interrupt int0_isr
+void int0_isr()
+{
+	if(INTCONbits.INT0IF==1)
+	{
+		flag = 1;
+		LED = ~LED;
+		INTCONbits.INT0IF = 0;
+	}
+
+}
+
+#pragma code HiPrio_int = 0x08
+void HiPrio_int()
+{
+	_asm
+		GOTO int0_isr
+	_endasm
+}
 
 void delay(int n)
 {
@@ -22,7 +52,14 @@ void pattern1()
 	int i;
 	for(i=0;i<=0xff;i++)	// Up counter
 	{
-		PORTB = i;
+		if(flag)
+		{
+			PORTD = 0;
+			sel++;
+			flag = 0;
+			break;
+		}	
+		PORTD = i;
 		delay(DEL);
 	}
 }
@@ -30,42 +67,58 @@ void pattern1()
 void pattern2()
 {
 	int i,j;
+	
 	for(i=0;i<=7;i++)
 	{
-		PORTB = 1<<i;
+		if(flag)
+		{
+			break;
+		}
+
+		PORTD = 1<<i;
 		delay(DEL);
 	}
 	for(i=7;i>=0;i--)
 	{
-		PORTB = 1<<i;
+		if(flag)
+		{
+			PORTD = 0;
+			sel++;
+			flag = 0;
+			break;
+		}
+
+		PORTD = 1<<i;
 		delay(DEL);
 	}	
 }
 
 void pattern3()
 {
-	PORTB = 0XAA;
+	if(flag)
+	{
+		PORTD = 0;
+		sel++;
+		flag = 0;
+	}
+	PORTD = 0XAA;
 	delay(DEL+5);
-	PORTB = 0X55;
+	PORTD = 0X55;
 	delay(DEL+5);
 }
 
 void main()
 {	
-	int sel;
-
-	TRISB = 0;
-	TRISD = 0x03;
+	TRISD = 0;
+	TRISBbits.RB7 = 0;
 	
+	inter_init();
+
 	while(1)
-	{
-		sel = 0;
-		
-		if(SW1==1)
-			sel |= 1;
-		if(SW2==1)
-			sel |= 2;
-		
+	{	
+		if(sel==3)
+			sel = 0;
+
 		switch(sel)
 		{
 			case 0:
