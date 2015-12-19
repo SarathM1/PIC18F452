beb488094c63c6ed160d332090167be510046c00
Wirewords-PC <sarathm00@gmail.com>
2015-12-19 15:37:28 +0530

    1 sec delay working

--- Code/timer0_inter.c
+++ Code/timer0_inter.c
@@ -4,6 +4,7 @@
 
 #define LED PORTDbits.RD0
 unsigned char check = 0;
+int cntr = 0;
 
 void high_isr(void);
 void timer_init();
@@ -24,20 +25,25 @@ void high_isr (void)
 	if (INTCONbits.TMR0IF)
 	{  
 		INTCONbits.TMR0IF = 0;                 
-		TMR0H = 0x85;        //Timer Reload to count 1s
-		TMR0L = 0xEE;                    
-		check = 1;
+		TMR0H = 0xF6;        //Timer Reload to count 1ms
+		TMR0L = 0x3C;                    
+		cntr++;
+		if(cntr==1000)
+		{
+			check = 1;
+			cntr = 0;
+		}
     }
 }
-	
+
 void timer_init()
 {
 	/*****************Setting Interrupt ********************/
 	INTCON = 0x20;                
 	/*****************Setting Timer ************************/
-	T0CON = 0x85;                 //set up timer0 - prescaler 1:64
-	TMR0H = 0x85;                 //Timer Reload to count 1s
-	TMR0L = 0xEE;                    
+	T0CON = 0x80;                 //set up timer0 - prescaler 1:2
+	TMR0H = 0xF6;                 //Timer Reload to count 1ms
+	TMR0L = 0x3C;                    
 	INTCONbits.GIE = 1;          //enable interrupts
 
 }
@@ -48,7 +54,8 @@ void main(void){
 	timer_init();
 	/*****************Main Program **************************/
 	while(1){
-		if (check == 1){
+		if (check == 1)
+		{
 			LED = ~LED;			// Turn On LED BackLight
 			check = 0;
 		}
